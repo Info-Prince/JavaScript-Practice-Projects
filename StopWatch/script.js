f1cbf2c019e69baac61b72f31f756d6ffe2d25fc
Prince Kumar <princeinfo053@gmail.com>
2024-03-17 16:12:25 +0000

    JavaScript Added into StopWatch

--- StopWatch/script.js
+++ StopWatch/script.js
@@ -1,9 +1,56 @@
 const countDown = document.querySelector('#countDown');
 const resetValue = document.querySelector('.resetValue')
 
-let updateCount = 2;
-countDown.innerHTML = `${updateCount}`
-console.log(countDown);
-countDown.setInterval(() => {
-    updateCount++;
-}, 1000);
+let startCount = 0;
+let intervalId ;
+
+
+//EventListener
+document.querySelector('.startBtn').addEventListener('click', startTimer )
+
+document.querySelector('.resetBtn').addEventListener('click', resetTimer )
+
+document.querySelector('.stopBtn').addEventListener('click', stopTimer )
+
+document.querySelector('.timeBtn').addEventListener('click', getTime )
+
+document.querySelector('.clearBtn').addEventListener('click', celarAll )
+
+
+//functions
+
+function startTimer ()  {
+    console.log('CountDown Start');
+
+    intervalId = setInterval( () => {
+        countDown.innerHTML = startCount++;
+    },1000)
+}
+
+function resetTimer ()  {
+    console.log('CountDown reset');
+
+    startCount = 0;
+    countDown.innerHTML = startCount;
+    clearInterval(intervalId);
+}
+
+function stopTimer ()   {
+    console.log('CountDown stopped');
+    clearInterval (intervalId);
+}
+
+function getTime () {
+    const newPara = document.createElement("p");
+    newPara.innerText =    `The stop time is ${startCount}`;
+    console.log(newPara);
+    resetValue.appendChild(newPara);
+}
+
+
+function celarAll ()    {
+    resetValue.innerHTML = "";
+    startCount = 0;
+    countDown.innerHTML = startCount;   
+    clearInterval(intervalId);
+}
